822f1f7ebe421b683bf455bdf512c04e9d1571d8
EmineBasheva <eminka23@abv.bg>
2014-09-07 19:22:49 +0300

    add background and ship images

--- shipInSpace.js
+++ shipInSpace.js
@@ -1,5 +1,20 @@
+// add canvas
 var canvas = document.getElementById("shipInSpace");
 var context = canvas.getContext("2d");
 
 context.fillStyle = "black";
 context.strokeStyle = "red";
+
+// background image
+var backgroundImage = new Image();
+backgroundImage.onload = function () {
+  context.drawImage(backgroundImage, -10, -10);
+};
+backgroundImage.src = "img/background.jpg";
+
+// ship image
+var shipImage = new Image();
+shipImage.onload = function(){
+  context.drawImage(shipImage, 480, 530);
+};
+shipImage.src = "img/spaceship1_final.png";

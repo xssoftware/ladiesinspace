ed6e2b8590ef3ac126ef857177a3bf64a6b57ebf
dsmitevaOOP <dilianasz@gmail.com>
2014-09-07 20:55:35 +0300

    Updating JS

--- shipInSpace.js
+++ shipInSpace.js
@@ -1,21 +1,14 @@
-<<<<<<< HEAD
+//add canvas
 var canvas = document.createElement("canvas");
-var ctx = canvas.getContext("2d");
-ctx.fillStyle = "black";
-ctx.strokeStyle = "red";
+var context = canvas.getContext("2d");
+context.fillStyle = "black";
+context.strokeStyle = "red";
 canvas.width = 900;
 canvas.height = 600;
 canvas.style.border = "1px solid black";
 canvas.style.display = "block";
 canvas.style.margin = "20px auto";
 document.getElementById("game").appendChild(canvas);
-=======
-// add canvas
-var canvas = document.getElementById("shipInSpace");
-var context = canvas.getContext("2d");
-
-context.fillStyle = "black";
-context.strokeStyle = "red";
 
 // background image
 var backgroundImage = new Image();
@@ -30,4 +23,4 @@ shipImage.onload = function(){
   context.drawImage(shipImage, 480, 530);
 };
 shipImage.src = "img/spaceship1_final.png";
->>>>>>> 822f1f7ebe421b683bf455bdf512c04e9d1571d8
+

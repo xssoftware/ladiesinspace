1d768391e5e66df3b96c299eb6cf20b1b59c1831
dsmitevaOOP <dilianasz@gmail.com>
2014-09-08 17:09:25 +0300

    Very last update on slow down

--- shipInSpace.js
+++ shipInSpace.js
@@ -47,7 +47,12 @@ var ship = {
     update: function (modifier) {
 
         if (38 in keysDown) { // Player holding up
-            ship.y -= ship.speed * modifier;
+            if (40 in keysDown) { // Player holding down
+                ship.y -= ship.speed * modifier / 4.5;
+            }
+            else {
+                ship.y -= ship.speed * modifier;
+            }
         }
         if (40 in keysDown) { // Player holding down
             ship.y -= ship.speed * modifier / 4.5;
